77642fb98ccdc7c2c673f5b96c1c04e29ad6a7f6
Dev Jezreel James <132325622+JezCruz@users.noreply.github.com>
2026-04-01 22:49:20 +0800

    Create test2.java
    sum of two numbers

---
+++ test2.java
@@ -0,0 +1,11 @@
+// sum of two numbers
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int sum = a + b;
+
+        System.out.println("Sum: " + sum);
+    }
+}

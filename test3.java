1748861ce1d61b582d2776efe722e0306b5ac383
Dev Jezreel James <132325622+JezCruz@users.noreply.github.com>
2026-04-01 22:50:25 +0800

    Create test3.java
    odd or even number

---
+++ test3.java
@@ -0,0 +1,13 @@
+// Odd or Even Number
+
+public class Main {
+    public static void main(String[] args) {
+        int number = 7;
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is Even");
+        } else {
+            System.out.println(number + " is Odd");
+        }
+    }
+}

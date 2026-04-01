32e551cdd70d7a6083ed307b29d67f16e7d6ad56
Dev Jezreel James <132325622+JezCruz@users.noreply.github.com>
2026-04-01 22:32:23 +0800

    Create test.java
    Java Data Types

---
+++ test.java
@@ -0,0 +1,7 @@
+// Java Data Types
+
+int age = 18;          // whole number
+double price = 19.99; // decimal
+char grade = 'A';     // single character
+boolean isJavaFun = true; // true or false
+String name = "Jez";  // text

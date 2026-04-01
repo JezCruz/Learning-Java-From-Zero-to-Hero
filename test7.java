a5f1b331a2871867a3f008f039618b0ed6df8f1a
Dev Jezreel James <132325622+JezCruz@users.noreply.github.com>
2026-04-01 22:56:54 +0800

    Create test7.java
    loop in java 1 to 10

---
+++ test7.java
@@ -0,0 +1,10 @@
+// loop from 1 to 10
+
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}

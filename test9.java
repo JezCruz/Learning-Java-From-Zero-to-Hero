7687202817d2eed8cb31ea11f634702e320541db
Dev Jezreel James <132325622+JezCruz@users.noreply.github.com>
2026-04-01 23:00:07 +0800

    Create test9.java
    positive, negative or zero checker in java

---
+++ test9.java
@@ -0,0 +1,16 @@
+// positive, negative or zero checker in java
+
+
+public class Main {
+    public static void main(String[] args) {
+        int number = -5;
+
+        if (number > 0) {
+            System.out.println("Positive");
+        } else if (number < 0) {
+            System.out.println("Negative");
+        } else {
+            System.out.println("Zero");
+        }
+    }
+}

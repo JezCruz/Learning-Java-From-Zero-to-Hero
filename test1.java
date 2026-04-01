98e256d26e184b1100876b1d15f2a906c8677d4d
Dev Jezreel James <132325622+JezCruz@users.noreply.github.com>
2026-04-01 22:47:56 +0800

    Create test1.java
    print name and age

---
+++ test1.java
@@ -0,0 +1,12 @@
+// print name and age
+
+
+public class Main {
+    public static void main(String[] args) {
+        String name = "Jez";
+        int age = 18;
+
+        System.out.println("My name is " + name);
+        System.out.println("I am " + age + " years old.");
+    }
+}

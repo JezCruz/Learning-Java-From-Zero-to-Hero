7f508e55900691c094008332a18fecca7e8bb68f
Dev Jezreel James <132325622+JezCruz@users.noreply.github.com>
2026-04-01 23:00:55 +0800

    Create test10.py
    find the bigger number

---
+++ test10.py
@@ -0,0 +1,17 @@
+// find the bigger number 
+
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 20;
+
+        if (a > b) {
+            System.out.println(a + " is bigger");
+        } else if (b > a) {
+            System.out.println(b + " is bigger");
+        } else {
+            System.out.println("Both are equal");
+        }
+    }
+}

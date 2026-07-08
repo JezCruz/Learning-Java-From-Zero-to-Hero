77259bd0bd438923d9ac9c211ab24706a7e34519
Jezreel James Cruz <jezreeljamescruz1@gmail.com>
2026-07-08 16:01:11 +0800

    Update HelloWorld.java/class

--- 01-Hello-World/HelloWorld.java
+++ 01-Hello-World/HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld {
    public static void main(String[] args) {
 System.out.println("Hello, World!");
+greet();
+}
+   public static void greet() {
+System.out.println("I'm Jez!");
 }
 }

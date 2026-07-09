7c762ba07e96a284c74184b3f0c2b6955d514ce6
Jezreel James Cruz <jezreeljamescruz1@gmail.com>
2026-07-09 20:35:14 +0800

    03-Math simple math using Java.

---
+++ 03-Numbers/Numbers.java
@@ -0,0 +1,10 @@
+public class Numbers {
+   public static void main(String[] args) {
+System.out.println("Hello Numbers!");
+math();
+}
+   public static void math() {
+int x = 70 * 7;
+System.out.println(x);
+}
+}
